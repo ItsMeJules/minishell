6b0fafaafec29354a177e21aabfc6944f62ec26b
Jules <jules.prn06@gmail.com>
2021-04-06 16:32:40 +0200

    Termcap use understood so the architecture is changed

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jules <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 15:16:20 by jules             #+#    #+#             */
-/*   Updated: 2021/04/06 15:35:11 by jules            ###   ########.fr       */
+/*   Updated: 2021/04/06 16:31:33 by jules            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <term.h>
 # include "constants.h"
 
+void	init_termcap();
 void	print_error(int id, char *infos);
 
 #endif
--- srcs/termcap/termcap.c
+++ srcs/termcap/termcap.c
@@ -1,19 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   termcap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jules <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/06 15:08:03 by jules             #+#    #+#             */
-/*   Updated: 2021/04/06 16:31:40 by jules            ###   ########.fr       */
+/*   Created: 2021/04/06 16:30:51 by jules             #+#    #+#             */
+/*   Updated: 2021/04/06 16:31:10 by jules            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h" 
+#include "minishell.h"
 
-int	main()
+int	init_termcap()
 {
-	init_termcap();
+	int		ret_ent;
+	char	*term;
+
+	term = getenv("TERM");
+	if (!term)
+	{
+		print_error(TERM_ENV_NOT_FOUND, NULL);
+		return (-1);
+	}
+	ret_ent = tgetent(NULL, term);
+	if (ret_ent == -1)
+	{
+		print_error(TERMCAP_DB_ACCESS, NULL);
+		return (-1);
+	}
+	else if (ret_ent == 0)
+	{
+		print_error(TERM_TYPE_NOT_DEFINED, term);
+		return (-1);
+	}
 	return (0);
 }
+
