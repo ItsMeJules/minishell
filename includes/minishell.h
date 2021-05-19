9f88a1163949c6f704cc2ce56d74e834e3457e32
Tom Vacherat <tvacherat@gmail.com>
2021-05-19 12:37:29 +0200

    export w dquote

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jules <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 15:16:20 by jules             #+#    #+#             */
-/*   Updated: 2021/05/19 12:15:54 by tvachera         ###   ########.fr       */
+/*   Updated: 2021/05/19 12:36:30 by tvachera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -215,6 +215,11 @@ bool		is_declaration(char *str);
 bool		is_joinable(char *str);
 bool		check_var(char *str);
 void		concat_var(t_list **env, char *var, char *val);
+
+/*
+**	BUILTINS/export3.c
+*/
+char		*join_for_export(t_env *data);
 
 /*
 **	BUILTINS/unset.c
